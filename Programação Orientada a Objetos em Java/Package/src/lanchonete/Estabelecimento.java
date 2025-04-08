c19ebffe60751ed1937102ff759d74a61eef83db
Gabriel <gabrielbsantana61@gmail.com>
2025-04-08 19:49:12 -0300

    commit diario

--- Programação Orientada a Objetos em Java/Package/src/lanchonete/Estabelecimento.java
+++ Programação Orientada a Objetos em Java/Package/src/lanchonete/Estabelecimento.java
@@ -1,5 +1,40 @@
 package lanchonete;
 
 public class Estabelecimento {
-    
+    public static void main(String[] args){
+        Cozinheiro cozinheiro = new Cozinheiro();
+        cozinheiro.adicionarLancheNoBalcao();
+        cozinheiro.adicionarSucoNoBalcao();
+        cozinheiro.adcionarComboNoBalcao();
+        cozinheiro.prepararLache();
+        cozinheiro.prepararVitamina();
+        cozinheiro.prepararCombo();
+        cozinheiro.selecionarIngredientesLache();
+        cozinheiro.SelecionarIngreidentesVitamina();
+        cozinheiro.lavarIngredientes();
+        cozinheiro.baterVitaminaLiquidificador();
+        cozinheiro.fritaIngredientesLache();
+
+        Atendente atendente = new Atendente();
+        atendente.servindoMesa();
+        atendente.pegarLancheCozinha();
+        atendente.receberPagamento();
+        atendente.trocarGas();
+        atendente.pegarPedidoBalcao();
+        
+        Almoxarife almoxarife = new Almoxarife();
+        almoxarife.contralarEntrada();
+        almoxarife.controlarSaida();
+        almoxarife.entregarIngredientes();
+        almoxarife.trocarGas();
+        
+
+        Cliente cliente = new Cliente();
+        cliente.escolherLanche();
+        cliente.fazerPedido();
+        cliente.pagarConta();
+        cliente.consultarSaldoAplicativo();
+        cliente.pegarPedidoBalcao();
+        
+    }
 }
