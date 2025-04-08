de7092d8ccba15fc42a634e3de7e40f7da26785e
Gabriel <gabrielbsantana61@gmail.com>
2025-04-08 19:37:54 -0300

    commit de segurança

--- Programação Orientada a Objetos em Java/Package/src/lanchonete/Cozinheiro.java
+++ Programação Orientada a Objetos em Java/Package/src/lanchonete/Cozinheiro.java
@@ -1,5 +1,48 @@
 package lanchonete;
 
 public class Cozinheiro {
-    
+    public void adicionarLancheNoBalcao() {
+        System.out.println("ADCIONANDO LANCHE NATURAL");
+    }
+    public void adicionarSucoNoBalcao(){
+        System.out.println("ADCIONANDO SUCO NO BALCÃO");
+    }
+    public void adcionarComboNoBalcao(){
+        adicionarLancheNoBalcao();
+        adicionarSucoNoBalcao();
+    }
+    public void prepararLache(){
+        System.out.println("PREPARANDO LACHE NATURAL");
+    }
+    public void prepararVitamina(){
+        System.out.println("PREPARANDO SUCO");
+    }
+    public void prepararCombo(){
+        prepararLache();
+        prepararVitamina();
+    }
+    public void selecionarIngredientesLache(){
+        System.out.println("SELECIONANDO INGREDIENTES DO LACHE NATURAL");
+    }
+    public void SelecionarIngreidentesVitamina(){
+        System.out.println("SELECIONANDO INGREDIENTES DO SUCO");
+    }
+    public void lavarIngredientes(){
+        System.out.println("LAVANDO IGREDIENTES");
+    }
+    public void baterVitaminaLiquidificador(){
+        System.out.println("BATENDO VITAMINA NO LIQUIDIFICADOR");
+    }
+    public void fritaIngredientesLache(){
+        System.out.println("FRITANDO A CARNE E O OVO");
+    }
+    public void pedirParaTrocarGas(Atendente meuAmigo){
+        meuAmigo.trocarGas();
+    }
+    public void pedirParaTrocarGas(Almoxarife meuAmigo){
+        meuAmigo.trocarGas();
+    }
+    public void pedirIgredientes(Almoxarife meuAmigo){
+        meuAmigo.entregarIngredientes();
+    }
 }
